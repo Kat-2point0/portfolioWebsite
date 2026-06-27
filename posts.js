c130630ba0247134eb4d6a3c92a8a6a7a42e1751
Kat-2point0 <kat2point00@gmail.com>
2026-06-27 14:35:25 +0200

    chore: add the posts to categories
    I'm making category pages for writing.html. I add posts in posts.js to these categories.

--- posts.js
+++ posts.js
@@ -5,37 +5,43 @@ const posts = [
         title: "The Brave New World We Chose | An essay",
         excerpt: "In the 21st century, we're not waiting for dystopia. We are already there...",
         link: "brave-new-world.html",
-        date: "2026-06-23"
+        date: "2026-06-23",
+        category: "essays"
     },
     {
         title: "Sonder II",
         excerpt: "I walk my dog at night. There's an apartment building just around the bend...",
         link: "sonder-ii.html",
-        date: "2026-06-22"
+        date: "2026-06-22",
+        category: "poetry"
     },
     {
         title: "Dina Derboven Part 1: The Crown | An Interview",
         excerpt: "Dina Derboven, Miss Environment Belgium 2025, is more than her title...",
         link: "dina-derboven.html",
-        date: "2026-06-21"
+        date: "2026-06-21",
+        category: "interviews"
     },
     {
         title: "Moonlight",
         excerpt: "Like the moon shines at night, enlightening the well, where dreams shine bright...",
         link: "moonlight.html",
-        date: "2026-06-20"
+        date: "2026-06-20",
+        category: "poetry"
     },
     {
         title: "Tensions | An Excerpt from The Mirror of Melodies",
         excerpt: "The week went by swiftly, with classes running as usual...",
         link: "tensions.html",
-        date: "2026-06-19"
+        date: "2026-06-19",
+        category: "excerpts"
     },
     {
         title: "Alone",
         excerpt: "I sing and sing, and from within I wish and wish for a mate to come...",
         link: "alone.html",
-        date: "2026-06-18"
+        date: "2026-06-18",
+        category: "poetry"
     }
 ];
 
